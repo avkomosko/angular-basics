77c8056d8133e85c4ba0dca4640c2daf661bb189
Aliaksei <avkomosko@gmail.com>
2022-02-12 20:51:24 +0300

    feat: add http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Todo } from './models/interfaces';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+  todos: Todo[] = [];
 
+  todoTitle = '';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .subscribe(todos => {
+      this.todos = todos;
+    })
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+    .subscribe(todo => {
+      console.log(todo);
+      this.todos.push(todo);
+      this.todoTitle = '';
+    })
   }
 }
