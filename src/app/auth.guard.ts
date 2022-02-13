ab787a53d88605df01177be9f5898cf3a0ef30bf
Aliaksei <avkomosko@gmail.com>
2022-02-13 15:46:12 +0300

    feat: add child auth guard

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './services/auth.service';
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
   constructor(
     private authService: AuthService,
     private router: Router
   ){}
+
+  canActivateChild( childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot ): boolean | Observable<boolean> | Promise<boolean> {
+    return this.canActivate( childRoute, state );
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
