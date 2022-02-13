7b7bf38e2b33e7fd11c3965dd4640f5cec282d16
Aliaksei <avkomosko@gmail.com>
2022-02-13 15:40:40 +0300

    feat: add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts/posts.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent, children: [
     {path: 'extra', component: AboutExtraComponent}
   ]},
-  {path: 'posts', component: PostsComponent},
+  {path: 'posts', component: PostsComponent, canActivate: [AuthGuard]},
   {path: 'posts/:id', component: PostComponent},
   {path: 'error', component: ErrorPageComponent},
   {path: '**', redirectTo: '/error'},
