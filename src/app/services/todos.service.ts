891e70dd99e09f14192b52c8d080677567031329
Aliaksei <avkomosko@gmail.com>
2022-02-13 11:11:59 +0300

    feat: add http params to request

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, delay } from 'rxjs/operators';
@@ -17,12 +17,16 @@ export class TodoService {
   }
 
   fetchTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .pipe(
+    let params = new HttpParams();
+    params.append('_limit', '4');
+    params.append('custom', 'anything');
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
+      // params: new HttpParams().set('_limit', '3')
+      params
+    }).pipe(
         delay(500),
         catchError((e) => {
-          console.log(e.message);
-          return throwError(e);
+        return throwError(e);
         })
       );
   }
