abc32941e71531f125fa3cd483cf7d4cc1227e29
Aliaksei <avkomosko@gmail.com>
2022-02-13 14:58:16 +0300

    feat: add query params

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,37 @@
-import {Component} from '@angular/core'
-import {PostsService} from '../services/post.service'
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostsService } from '../services/post.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit {
+  showIds = false;
+
+  constructor(
+    public postsService: PostsService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params)=> {
+      this.showIds = !!params[ 'showIds' ]
+    });
+
+    this.route.fragment.subscribe((params)=> {
+      console.log(params);
+    })
+  }
+
+  showIdsProgram() {
+    this.router.navigate(['/posts'], {
+      queryParams: {
+        showIds: true
+      },
+      fragment: 'fragment'
+    });
+  }
 }
